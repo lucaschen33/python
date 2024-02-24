624b264318cfdcf17c2fd9e7ed71ee3d7dafd3b3
lucachen33 <johndoe@email.com>
2024-02-24 12:40:52 -0500

    muliplication final

--- Big-book-of-py-projects/multiplication.py
+++ Big-book-of-py-projects/multiplication.py
@@ -2,8 +2,8 @@ print('  |  0   1   2   3   4   5   6   7   8   9  10  11  12')
 print('--+---------------------------------------------------')
 
 for number1 in range(0, 13):
-    print(str(number1).rjust(2), end= '')
-    print('|', end = '')
+    print(str(number1).rjust(2), end='')
+    print('|', end='')
     for number2 in range(0, 13):
-        print(str(number1 * 2).rjust(3), end = '')
+        print(str(number1 * number2).rjust(3), end=' ')
     print()
