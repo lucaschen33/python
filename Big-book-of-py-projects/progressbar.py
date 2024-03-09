f58e79644ae2767120bd159f7735cc619f962bc1
lucachen33 <johndoe@email.com>
2024-03-08 20:24:57 -0500

    progressbar final

---
+++ Big-book-of-py-projects/progressbar.py
@@ -0,0 +1,34 @@
+import random, time
+
+BAR = chr(9608)
+
+def main():
+    print('Progress Bar Simulation, by Al Sweigart')
+    bytesDownloaded = 0
+    downloadSize = 4096
+    while bytesDownloaded < downloadSize:
+        bytesDownloaded += random.randint(0, 100)
+        barStr = getProgressBar(bytesDownloaded, downloadSize)
+        print(barStr, end='', flush=True)
+        time.sleep(0.2)
+        print('\b' * len(barStr), end='', flush=True)
+        
+def getProgressBar(progress, total, barWidth=40):
+    progressBar = ''
+    progressBar += '['
+    if progress > total:
+        progress = total
+    if progress < 0:
+        progress = 0
+        
+    numberOfBars = int((progress/ total)*barWidth)   
+    progressBar += BAR*numberOfBars
+    progressBar += ' ' *(barWidth - numberOfBars) 
+    progressBar += ']' 
+    percentComplete = round(progress / total*100, 1)
+    progressBar +=' ' + str(percentComplete) + '%'
+    progressBar += ' ' + str(progress) + '/' + str(total)
+    return progressBar
+
+if __name__ == '__main__':
+    main()
